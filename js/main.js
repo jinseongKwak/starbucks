a6ff3dd8c4a50fd67168b38eae7227a840d3675f
jinseongKwak <gorden42@naver.com>
2022-11-16 16:45:04 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-//검색버튼
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', "통합검색");
-});
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', "");
-});
-
 //badges 로데시 and gsap
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -144,10 +129,6 @@ spyEls.forEach(function (spyEl) {
       .setClassToggle(spyEl, 'show')
       .addTo(new ScrollMagic.Controller());
 }); 
-
-/* footer year */
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
 
 
